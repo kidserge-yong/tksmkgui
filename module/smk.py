5e1746599fa9d2e453e0de234b9182d386f3413a
Sorawit Stapornchaisit <46063093+kidserge-yong@users.noreply.github.com>
2021-09-06 16:03:01 +0900

    Update smk.py

--- module/smk.py
+++ module/smk.py
@@ -15,6 +15,7 @@ DEBUG = False
 def threaded(fn):
     def wrapper(*args, **kwargs):
         thread = Thread(target=fn, args=args, kwargs=kwargs)
+        thread.daemon = True
         thread.start()
         return thread
     return wrapper
